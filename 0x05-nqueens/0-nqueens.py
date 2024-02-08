bf23276238afac85cb0b2d27c707ec3e3dc9759e
Obi William <williamobi818@gmail.com>
2024-02-08 23:20:25 +0100

    chess queen test 3

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,50 +1,58 @@
 #!/usr/bin/python3
-"""
-N queens puzzle
-"""
-
+"""Solve the famous N Queen Problem"""
 import sys
 
 
-def is_NQueen(cell: list) -> bool:
-    """ False if not N Queen, True if N Queen """
-    row_number = len(cell) - 1
-    difference = 0
-    for index in range(0, row_number):
-        difference = cell[index] - cell[row_number]
-        if difference < 0:
-            difference *= -1
-        if difference == 0 or difference == row_number - index:
-            return False
-    return True
-
-
-def solve_NQueens(dimension: int, row: int, cell: list, output: list):
-    """ Return result of N Queens recursively """
-    if row == dimension:
-        print(output)
-    else:
-        for column in range(0, dimension):
-            cell.append(column)
-            output.append([row, column])
-            if (is_NQueen(cell)):
-                solve_NQueens(dimension, row + 1, cell, output)
-            cell.pop()
-            output.pop()
-
-
 if len(sys.argv) != 2:
-    print('Usage: nqueens N')
-    exit(1)
+    print("Usage: nqueens N")
+    sys.exit(1)
+
+
+def print_error(message):
+    """Print the error message and exit with status 1"""
+    print(message)
+    sys.exit(1)
+
+
 try:
-    N = int(sys.argv[1])
-except BaseException:
-    print('N must be a number')
-    exit(1)
-if N < 4:
-    print('N must be at least 4')
-    exit(1)
-else:
-    output = []
-    cell = 0
-    solve_NQueens(int(N), cell, [], output)
+    n = int(sys.argv[1])
+except ValueError:
+    print_error("N must be a number")
+
+if n < 4:
+    print_error("N must be at least 4")
+
+col = set()
+# (r - c)
+positive_diagonal = set()
+# (r + c)
+negative_diagonal = set()
+# The original board
+board = []
+
+
+def backtracking(r):
+    """Backtracking function"""
+    if r == n:
+        print(board)
+        return
+    for c in range(n):
+        if c in col or (r - c) in positive_diagonal or \
+                (r + c) in negative_diagonal:
+            continue
+
+        col.add(c)
+        positive_diagonal.add(r - c)
+        negative_diagonal.add(r + c)
+
+        board.append([r, c])
+
+        backtracking(r + 1)
+
+        col.remove(c)
+        positive_diagonal.remove(r - c)
+        negative_diagonal.remove(r + c)
+        board.pop()
+
+
+backtracking(0)
