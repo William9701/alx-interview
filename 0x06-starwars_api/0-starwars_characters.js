bcc48eb65537b2d44b3ec6e214e347d30d4fe177
Obi William <williamobi818@gmail.com>
2024-02-15 23:05:01 +0100

    lfy

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const req = require('request');
+const req = require("request");
 const parameters = process.argv;
 
 const retrieveCharacter = (url) => {
@@ -17,23 +17,20 @@ const retrieveCharacter = (url) => {
 
 if (parameters.length === 3) {
   req(
-`https://swapi-api.alx-tools.com/api/films/${parameters[2]}`,
-(error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const chars = JSON.parse(body).characters;
+    `https://swapi-api.alx-tools.com/api/films/${parameters[2]}`,
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const chars = JSON.parse(body).characters;
 
-    const retrievePromises = chars.map((url) =>
-      retrieveCharacter(url)
-    );
-    Promise.all(retrievePromises)
-      .then((names) => {
-        names.forEach((name) => console.log(name));
-      })
-      .catch((error) => console.error('Error:', error));
-  } else {
-    console.error('Error:', error);
-  }
-}
+        const retrievePromises = chars.map((url) => retrieveCharacter(url));
+        Promise.all(retrievePromises)
+          .then((names) => {
+            names.forEach((name) => console.log(name));
+          })
+          .catch((error) => console.error("Error:", error));
+      } else {
+        console.error("Error:", error);
+      }
+    }
   );
 }
-
